f1b6de62874ce3d7cf67d987dce56f13e748adf5
Ishan Khanna <ishan1604@gmail.com>
2015-11-12 17:25:27 +0530

    Added: File Template for AsyncTask

---
+++ fileTemplates/AsyncTask.java
@@ -0,0 +1,24 @@
+package ${PACKAGE_NAME};
+
+import android.os.AsyncTask;
+
+/**
+ * An asynchronous task.
+ */
+public class ${NAME} extends AsyncTask<Void,Void,Void> {
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
